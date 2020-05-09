b3c64184b9396c8a78af6b5cf98a825d280f8837
fgken <fg.bell.tree.333@gmail.com>
2020-05-10 00:42:07 +0900

    daemon: fix syslog ident

--- src/daemon.c
+++ src/daemon.c
@@ -6,7 +6,7 @@
 int
 daemon_main(int argc, char *argv[])
 {
-    log_init(LOG_OUTPUT_STDERR | LOG_OUTPUT_SYSLOG, NULL, LOG_DAEMON);
+    log_init(LOG_OUTPUT_STDERR | LOG_OUTPUT_SYSLOG, "daemon", LOG_DAEMON);
     log_info("Starting daemon...");
     return 0;
 }
